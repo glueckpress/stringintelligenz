c75f71606a8eac0e88729465636f1beac9ff0fc2
Caspar Hübinger <caspar@glueckpress.com>
2016-09-24 20:05:59 +0200

    Onboarding für nicht unterstützte Locales #12

--- src/StringintelligenzAdminNotice.php
+++ src/StringintelligenzAdminNotice.php
@@ -18,12 +18,12 @@ class StringintelligenzAdminNotice {
 	public function render() {
 
 		?>
-		<div class="notice notice-error is-dismissible">
-			<p>
-				<?php
+		<div class="notice notice-warning is-dismissible">
+			<h4><?php _e( '🙅✋👁📢&#160;Achtung! Stringintelligenz only supports default (<dfn title="That’s de_DE as a locale.">informal</dfn>) German for now.', 'stringintelligenz' ); ?></h4>
+			<p><?php
 				printf(
 					__(
-						'<strong>Stringintelligenz</strong> ist momentan nur für <em>Deutsch</em> verfügbar („Du“-Version, Locale: <code>de_DE</code>). Unter <a href="%s">Einstellungen→Allgemein</a> kannst du zu <em>Deutsch</em> wechseln.',
+						'Switch to <strong>“Deutsch”</strong> in <a href="%s">Settings→General→Site&#160;Language</a> in order to enable gender-sensitive German in your WordPress admin interface.<br><em>(Quick check: As an administrator you should see the word “Profil” instead of “Benutzer” in your admin menu after you have activated Deutsch.)</em>',
 						'stringintelligenz'
 					),
 					admin_url( 'options-general.php' )
